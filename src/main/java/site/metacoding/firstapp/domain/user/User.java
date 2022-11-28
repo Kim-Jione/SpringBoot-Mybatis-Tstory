faaf09a249c782d08c7a0df6a7c63c14a626b632
Kimjiwon <hghg1036@naver.com>
2022-11-28 11:20:06 +0900

    nickname 칼럼 추가완료

--- src/main/java/site/metacoding/firstapp/domain/user/User.java
+++ src/main/java/site/metacoding/firstapp/domain/user/User.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	private Integer userId;
 	private String username;
+	private String nickname;
 	private String password;
 	private String email;
 	private String profileImg;
