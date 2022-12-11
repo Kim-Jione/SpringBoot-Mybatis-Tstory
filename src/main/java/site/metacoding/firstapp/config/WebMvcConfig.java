67f726b974d18e6fee05303b8b7535aaca168d36
Kimjiwon <hghg1036@naver.com>
2022-12-11 19:54:43 +0900

    인터셉터 오류 수정완료

---
+++ src/main/java/site/metacoding/firstapp/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package site.metacoding.firstapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import site.metacoding.firstapp.handler.LoginIntercepter;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		registry.addInterceptor(new LoginIntercepter())
+		.addPathPatterns("/s/**");
+		//.addPathPatterns("/admin/**")
+		//.excludePathPatterns("/s/boards/**"); // /s/* => /s/boards, s/users     XXXX /s/boards/1
+	}
+}
